49441a6177e4d03217e174988898b7feecba1550
Adam Breaux <breauxaj@gmail.com>
2014-06-03 12:23:23 -0400

    need to rethink

--- lib/puppet/provider/mysql_user/ruby.rb
+++ lib/puppet/provider/mysql_user/ruby.rb
@@ -1,16 +1,6 @@
 Puppet::Type.type(:mysql_user).provide(:ruby) do
   commands :mysqladmin => 'mysqladmin'
 
-  def get_mysql_users(int)
-    begin
-      output = mysqladmin(['-getproxybypassdomains', int])
-    rescue Puppet::ExecutionFailure => e
-      Puppet.debug("#get_mysql_users had an error -> #{e.inspect}")
-      return nil
-    end
-    users = output.split("\n").sort
-    return nil if users.first =~ /There aren\'t any bypass domains set/
-    users
-  end
+
 
 end
