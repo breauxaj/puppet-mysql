ba4730b44552634ec93bbaf28553804b56b437ba
Adam Breaux <breauxaj@gmail.com>
2014-06-13 13:41:30 -0400

    wip

--- lib/puppet/provider/mysql_database/ruby.rb
+++ lib/puppet/provider/mysql_database/ruby.rb
@@ -1,3 +1,18 @@
 Puppet::Type.type(:mysql_database).provide(:ruby) do
-  # Provider work goes here
+  desc "Manage databases, none of this will work without a .my.cnf file"
+
+  commands :mysql => 'mysql', :mysqladmin => 'mysqladmin'
+
+  def create
+
+  end
+
+  def destroy
+
+  end
+
+  def exists?
+    mysql 'show databases'
+  end
+
 end
